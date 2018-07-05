e58842fdd121f1113443bc2635aa6e4652b3cee7
Kajigga Dev <kajigga@gmail.com>
2018-07-05 16:24:30 -0600

    change config reference

--- salt/cli/files/etc/profile.d/prompt.sh
+++ salt/cli/files/etc/profile.d/prompt.sh
@@ -1,4 +1,4 @@
-{% set minion_opts = salt.config.minion_config('/etc/salt/minion') %}
+{% set minion_opts = salt.config %}
 export NICKNAME={{ minion_opts['id'] }}
 if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$NICKNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
