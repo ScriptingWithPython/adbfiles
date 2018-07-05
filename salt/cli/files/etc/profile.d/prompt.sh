3e41d10600830974a6992d87949f3e4ec3da38fe
Kajigga Dev <kajigga@gmail.com>
2018-07-05 16:27:37 -0600

    put id from grain in file

--- salt/cli/files/etc/profile.d/prompt.sh
+++ salt/cli/files/etc/profile.d/prompt.sh
@@ -1,5 +1,4 @@
-{% set minion_opts = salt.config %}
-export NICKNAME={{ minion_opts['id'] }}
+export NICKNAME={{ salt['grains.get']('id') }}
 if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$NICKNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
