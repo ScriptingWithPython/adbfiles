5b16186d4d6f576d4ba185c54c3e44a204eaaf51
Kajigga Dev <kajigga@gmail.com>
2018-07-05 16:19:02 -0600

    jinja and templating

--- salt/cli/files/etc/profile.d/prompt.sh
+++ salt/cli/files/etc/profile.d/prompt.sh
@@ -1,4 +1,5 @@
-export NICKNAME=adobe-itcloud
+{% set minion_opts = salt.config.minion_config('/etc/salt/minion') %}
+export NICKNAME={{ minion_opts['id'] }}
 if [ "$color_prompt" = yes ]; then
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$NICKNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
